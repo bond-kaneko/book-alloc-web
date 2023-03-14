5b0bb710e6583c99371cefece2c705c25c575b9a
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-14 18:07:25 +0900

    put login form

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const valid = ref(false)
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>Login</h1>
+    </div>
+    <div>
+      <v-form v-model="valid">
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="email"></v-text-field>
+            <v-text-field v-model="password"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn type="submit">Login</v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
