6b2db1e7cb000e14f838f48d3b7d23a9c6ba9290
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-15 09:37:28 +0900

    generate ssr mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   target: 'static',
   ...routerBase,
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
