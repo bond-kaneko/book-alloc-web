a4e3aa3d5dc6151432a5028d273ad24a56e88ad5
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-15 14:38:31 +0900

    put book images

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,5 +1,81 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">hoge</div>
+  <div class="container">
+    <div class="notification is-link is-light">
+      ランディングページです。<br />ポートフォリオの円グラフと、読書中の本を表示します。
+    </div>
+    <section class="section"><h1 class="title">ページタイトル</h1></section>
+    <section class="section">ポートフォリオの円グラフ</section>
+    <section class="section">
+      <h2 class="title">読んでる本</h2>
+      <ul class="columns is-desktop">
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img
+                  src="http://image.gihyo.co.jp/assets/images/cover/2022/9784297131111.jpg"
+                />
+              </figure>
+            </div>
+          </div>
+        </li>
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img
+                  src="https://gihyo.jp/assets/images/cover/2023/thumb/TH320_9784297133986.jpg"
+                />
+              </figure>
+            </div>
+          </div>
+        </li>
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img src="https://www.iwanami.co.jp//images/book/621636.jpg" />
+              </figure>
+            </div>
+          </div>
+        </li>
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img
+                  src="https://gihyo.jp/assets/images/cover/2023/thumb/TH320_9784297134723.jpg"
+                />
+              </figure>
+            </div>
+          </div>
+        </li>
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img src="https://www.iwanami.co.jp//images/book/621803.jpg" />
+              </figure>
+            </div>
+          </div>
+        </li>
+        <li class="column">
+          <div class="card mx-3">
+            <div class="card-image">
+              <figure class="image is-4by5">
+                <img
+                  src="https://gihyo.jp/assets/images/cover/2023/thumb/TH320_9784297134471.jpg"
+                />
+              </figure>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div class="link is-flex is-justify-content-center">
+        <button class="button is-size-3">もっと見る</button>
+      </div>
+    </section>
+  </div>
 </template>
