3d3f84d6d6d1e709eddbff73bd397c6510156fd0
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-20 17:05:02 +0900

    add handle method

--- src/pages/users/UserIndex.vue
+++ src/pages/users/UserIndex.vue
@@ -1,27 +1,30 @@
 <script setup lang="ts">
-import {useAuth0} from '@auth0/auth0-vue';
-import {getWithAuth} from "../../auth0";
+import { useAuth0 } from "@auth0/auth0-vue";
+import { getWithAuth } from "../../auth0";
 
-const {loginWithRedirect, logout, user, isAuthenticated} = useAuth0();
-const login = () => {
+const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+const handleLogin = () => {
   loginWithRedirect();
-}
+};
+const handleLogout = () => {
+  logout();
+};
 
-const publicPing = await getWithAuth("http://localhost:8888/public")
-const privatePing = await getWithAuth("http://localhost:8888/auth/ping")
+const publicPing = await getWithAuth("http://localhost:8888/public");
+const privatePing = await getWithAuth("http://localhost:8888/auth/ping");
 </script>
 
 <template>
-    <div class="container">
-      <h2>User Profile</h2>
-      <button @click="login">Log in</button>
-      <button @click="logout">Log out</button>
+  <div class="container">
+    <h2>User Profile</h2>
+    <button @click="handleLogin()">Log in</button>
+    <button @click="handleLogout">Log out</button>
 
-      <p>Public ping: {{publicPing.data}}</p>
-      <p>Private ping: {{privatePing.data}}</p>
-      <p>Auth: {{ isAuthenticated }}</p>
-      <pre v-if="isAuthenticated">
+    <p>Public ping: {{ publicPing.data }}</p>
+    <p>Private ping: {{ privatePing.data }}</p>
+    <p>Auth: {{ isAuthenticated }}</p>
+    <pre v-if="isAuthenticated">
         <code>{{ user }}</code>
       </pre>
-    </div>
+  </div>
 </template>
