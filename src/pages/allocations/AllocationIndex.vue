65a9e9c04b9910149e9d5de8ff888446238aae93
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-22 17:16:03 +0900

    create allocation

--- src/pages/allocations/AllocationIndex.vue
+++ src/pages/allocations/AllocationIndex.vue
@@ -2,12 +2,26 @@
 import { useAuth0 } from '@auth0/auth0-vue';
 import { postWithAuth } from '../../auth0';
 
-const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+const { user } = useAuth0();
 
 const handleCreate = async () => {
+  const loginUser = await postWithAuth(
+    import.meta.env.VITE_API_URL + '/auth/users/me',
+    {
+      auth0Id: user.value.sub,
+      email: user.value.email,
+      name: user.value.nickname,
+    },
+    {}
+  );
   await postWithAuth(
     import.meta.env.VITE_API_URL + '/auth/allocations/',
-    {},
+    {
+      userId: loginUser.data.ID,
+      name: user.value.name,
+      share: 1,
+      isActive: true,
+    },
     {}
   );
 };
