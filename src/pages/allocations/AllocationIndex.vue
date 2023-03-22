9aba3bf3ade93a6d32908e6394bd51388e69f751
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-22 12:52:38 +0900

    ping pong

--- src/pages/allocations/AllocationIndex.vue
+++ src/pages/allocations/AllocationIndex.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+import { postWithAuth } from '../../auth0';
+
+const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+const handleCreate = async () => {
+  await postWithAuth(
+    import.meta.env.VITE_API_URL + '/auth/allocations/',
+    {},
+    {}
+  );
+};
+</script>
 
 <template>
-  <p>allocation</p>
+  <div class="containter">
+    <h1>allocation</h1>
+    <button class="button" @click="handleCreate()">New</button>
+  </div>
 </template>
