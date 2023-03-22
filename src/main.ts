b845b6a97f774136dfafa7dc8183886bcb5855eb
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-22 12:32:46 +0900

    put empty allocation page

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,11 @@
 import {createApp} from 'vue'
 import {createRouter, createWebHistory} from "vue-router";
 import App from "./App.vue";
-import Test from "./pages/Test.vue";
-import UserIndex from "./pages/users/UserIndex.vue";
-import {authGuard} from "@auth0/auth0-vue";
-import Login from "./pages/Login.vue";
-import Logout from "./pages/Logout.vue";
 import { createPinia } from 'pinia'
 import {auth0} from "./auth0";
+import { routes } from './route';
 
-const routes = [
-    {
-        path: '/',
-        component: Test
-    },
-    {
-        path: '/login',
-        component: Login
-    },
-    {
-        path: '/logout',
-        component: Logout
-    },
-    {
-        path: '/users',
-        component: UserIndex,
-        beforeEnter: authGuard,
 
-    },
-]
 const router = createRouter({
     history: createWebHistory(),
     routes,
---
+++ src/route.ts
@@ -0,0 +1,31 @@
+import UserIndex from './pages/users/UserIndex.vue';
+import { authGuard } from '@auth0/auth0-vue';
+import Login from './pages/Login.vue';
+import Logout from './pages/Logout.vue';
+import Test from './pages/Test.vue';
+import AllocationIndex from './pages/allocations/AllocationIndex.vue';
+
+export const routes = [
+  {
+    path: '/',
+    component: Test,
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/logout',
+    component: Logout,
+  },
+  {
+    path: '/users',
+    component: UserIndex,
+    beforeEnter: authGuard,
+  },
+  {
+    path: '/allocations',
+    component: AllocationIndex,
+    beforeEnter: authGuard,
+  },
+];
