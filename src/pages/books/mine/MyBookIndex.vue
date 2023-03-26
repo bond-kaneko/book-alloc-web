0da4331e3a5778e67ccd35ff7e03cb27e47c1e77
junki.kaneko <junki.kaneko@linecorp.com>
2023-03-26 21:48:56 +0900

    change path

--- src/pages/books/mine/MyBookIndex.vue
+++ src/pages/books/mine/MyBookIndex.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { useAuth0 } from '@auth0/auth0-vue';
-import { getWithAuth } from '../../../auth0';
+import { getWithAuth, postWithAuth } from '../../../auth0';
 
 const { user } = useAuth0();
+const loginUser = await postWithAuth(
+  import.meta.env.VITE_API_URL + '/auth/users/me',
+  {
+    auth0Id: user.value.sub,
+  },
+  {}
+);
 
 const getMyBooks = async () => {
   const response = await getWithAuth(
-    import.meta.env.VITE_API_URL + '/auth/books/mine'
+    import.meta.env.VITE_API_URL + '/auth/books/' + loginUser.data.ID
   );
   const data = await response.data;
   return data;
